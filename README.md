ae53ddf8227b2daa5467510649705851b59d43ac
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-06-30 08:35:20 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# RKB1 Mechanical Keyboard Firmware
+
+The stock firmware that comes with the RKB1
+
+## Authors
+
+[Charlotte](https://git.chir.rs/darkkirb)
+
+## License
+
+This project is licensed under the Apache 2.0 License - see the LICENSE.md file for details
+
---
+++ flake.nix
@@ -0,0 +1,70 @@
+{
+  description = "rust-template";
+
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs/nixpkgs-unstable;
+    flake-utils.url = github:numtide/flake-utils;
+
+    rust-overlay = {
+      url = github:oxalica/rust-overlay;
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+
+    cargo2nix = {
+      url = github:cargo2nix/cargo2nix;
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+      inputs.rust-overlay.follows = "rust-overlay";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, cargo2nix, ... } @ inputs: flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
+    let
+      overlays = [
+        cargo2nix.overlays.default
+        (import rust-overlay)
+      ];
+      pkgs = import nixpkgs {
+        inherit system overlays;
+      };
+      rustPkgs = pkgs.rustBuilder.makePackageSet {
+        packageFun = import ./Cargo.nix;
+        rustChannel = "nightly";
+        target = "thumbv6m-none-eabi";
+        packageOverrides = pkgs: pkgs.rustBuilder.overrides.all;
+      };
+    in
+    rec {
+      devShells.default = with pkgs; mkShell {
+        buildInputs = [
+          (rust-bin.nightly.latest.default.override {
+            extensions = [ "rust-src" ];
+          })
+          cargo2nix.packages.${system}.cargo2nix
+        ];
+      };
+      packages = { 
+        rkbfirm-source = pkgs.releaseTools.sourceTarball {
+          name = "rkbfirm-source";
+          src = self;
+          officialRelease = true;
+          version = self.lastModifiedDate;
+          nativeBuildInputs = [ pkgs.zstd ];
+          distPhase = ''
+            releaseName=rkb1-src-$version
+            mkdir -p $out/tarballs
+            mkdir ../$releaseName
+            cp -prd . ../$releaseName
+            (cd .. && tar -cf- $releaseName | zstd --ultra -22 > $out/tarballs/$releaseName.tar.zst) || false
+          '';
+        };
+        rust-template = rustPkgs.workspace.rust-template { };
+
+      };
+      nixosModules.default = import ./nixos {
+        inherit inputs system;
+      };
+      hydraJobs = packages;
+    });
+}
